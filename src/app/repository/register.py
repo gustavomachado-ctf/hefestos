30aa574b20462f91b62c42b2d51bce133bd6654a
cahlos <gustavo.machado@cotefacil.com>
2026-07-07 17:25:43 -0300

    feat: implementar processo de cadastro de integracoes

--- src/app/repository/register.py
+++ src/app/repository/register.py
@@ -33,7 +33,7 @@ class RegisterRepository:
             self.session.commit()
             self.session.refresh(model)
 
-            return model
+            return list(model) or None
 
         except Exception:
             self.session.rollback()
